48f1cd3c69d71a6d7e8d7dbcc5d41295d238d3a5
tim_g <hecongshen@global.com>
2020-04-22 21:52:32 +0800

    1

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+  ],
+  rules: {
+    "linebreak-style": [0 ,"error", "windows"],
+    'no-console': 'off',
+    'no-plusplus':'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'no-param-reassign': 'off',
+  },
+};
